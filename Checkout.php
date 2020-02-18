b492c1f9dd7719ad28ce618f7103a958e5895bda
Arikaim <sales@intersoft.biz>
2020-02-18 18:04:42 +0200

    =

---
+++ Checkout.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c)  Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Modules\Checkout;
+
+use Arikaim\Core\Extension\Module;
+
+/**
+ * Checkout module class
+ */
+class Checkout extends Module
+{   
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Install module
+     *
+     * @return void
+     */
+    public function install()
+    {
+        $this->installDriver('Arikaim\\Modules\\Checkout\\Driver\\PayPalCheckoutDriver');
+        
+        return true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+## Arikaim CMS Checkout Module
+![1.0.0](https://img.shields.io/github/release/arikaim/checkout-module.svg)
+![GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)
+
+
+
+Arikaim CMS Checkout module
+
+
+
+#### Requirements 
+  * PHP 7.1
+  * [Arikaim CMS](https://github.com/arikaim/arikaim)
+
+
+
+#### Installation
+
+```sh
+composer require arikaim/checkout-module
+```
