4f0406838d5d5ac2621f8063b8b4c4cf461565b8
Arikaim <info@arikaim.com>
2023-09-26 10:26:37 +0300

    =

--- subscriptions/paypal/Subscription.php
+++ subscriptions/paypal/Subscription.php
@@ -81,7 +81,7 @@ class Subscription extends SubscriptionApi implements SubscriptionInterface
         $agreement
             ->setName($title)
             ->setDescription($description)
-         //   ->setStartDate($startDate)
+            ->setStartDate($startDate)
             ->setPlan($plan)
             ->setPayer($payer);
        
